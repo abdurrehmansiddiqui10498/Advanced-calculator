f76f11b9989ea6c856a5b38c70e2b17bb855885b
Abdur Rehman Siddiqui <abisbah2011@gmail.com>
2024-06-16 15:50:04 +0500

    Create Advanced Calculator.py

---
+++ Advanced Calculator.py
@@ -0,0 +1,12 @@
+a = int(input( "Enter the first number: "))
+b = int(input( "Enter the second number: "))
+f = input( "Enter the operator: ")
+
+if f == "+":
+  print(a+b)
+elif f == "-":
+  print(a-b)
+elif f == "*":
+  print(a*b)
+elif f == "/":
+  print(a/b)
